eea354e5de30969e5d27133a714e681593865791
figgyfigs <a.figs@protonmail.com>
2020-08-15 15:08:47 -0700

    Global App Controller functions added.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////// Budget Controller
 var budgetController = (function() {
-	
+
 
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ///////////////////////// UI Controller
 var UIController = (function() {
-	
+
 	var DOMstrings = {
-		
+
 		inputType: '.add__type',
 		inputDescription: 'add__description',
 	}
@@ -20,11 +20,51 @@ var UIController = (function() {
 ////////////////////////////////////////////////////////////////////
 ////////////////Global App Controller
 var Controller = (function(budgetCtrl, UICtrl) {
-	
+
 	var setupEventListeners = function() {
 
-	var DOM = UICtrl.getDOMStrings();
-	
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-	
-}
+			var DOM = UICtrl.getDOMStrings();
+
+			document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+			document.addEventListener('keypress', function(event) {
+					if(event.keycode === 13 || event.which === 13) {
+						ctrlAddItem();
+					}
+			});
+
+			document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+      document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+
+		};
+
+
+		var updateBudget = function() {
+
+	    //input = UICtrl.getInput();
+	    //      1. Calculate the budget
+	    budgetCtrl.calculateBudget();
+
+	    //      2. return the budget
+	    var budget = budgetCtrl.getBudget();
+
+	    //      3. Display the budget on the UI
+	    UICtrl.displayBudget(budget);
+
+	  };
+
+	  var updatePercentages = function() {
+
+	    // 1. calculate percentages
+	    budgetCtrl.calculatePercentages();
+
+
+	   	// 2. read percentages from the budget controller
+	   	var percentages = budgetCtrl.getPercentages();
+
+
+	   	// 3. update the UI with the new percentages
+	   	UICtrl.displayPercentages(percentages);
+
+		};
