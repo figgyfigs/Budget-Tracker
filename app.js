6e6e16aa49cb2fe758edce5466de95e6b75ad760
figgyfigs <a.figs@protonmail.com>
2020-08-28 13:13:51 -0700

    Calculate percentage

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 //////////////////////////// Budget Controller
 var budgetController = (function() {
 
+	var Expense = function(id, description, value) {
+
+    this.id = id;
+    this.description = description;
+    this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+
+    if(totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+
 
 }
 
